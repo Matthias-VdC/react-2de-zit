c00f2824fc6a3c157d39e112dd3da2d298cfbfc4
MatthiasVandeCasteele <matthias.van.de.casteele@student.ehb.be>
2022-07-07 13:33:10 +0200

    Fixed z-index issues with header

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Post(props: any) {
+  const [page, setPage] = useState(1);
+  const [scrollState, setScrollState] = useState("scrollIn");
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      if (window.scrollY === 0) {
+        setPage(1);
+      }
+    });
+  }, []);
+
   return (
     <div className="post-container">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="post-header"></div>
+      <div className="post-body"></div>
+      <div className="post-footer"></div>
     </div>
   );
 }
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -2,10 +2,9 @@ import Post from "../components/Post";
 
 export default function Home() {
   return (
-    <div>
-      <div id="home-post-container">
-        <Post />
-      </div>
+    <div id="home-post-container">
+      <Post />
+      <Post />
     </div>
   );
 }
